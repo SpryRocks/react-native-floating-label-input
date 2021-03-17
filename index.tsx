068b0ef56bfe4c04b3e683e9e65c3203f35af536
maximzhemerenko <maxim.zhemerenko@outlook.com>
2021-03-18 00:59:31 +0200

    Remove onSelectionChange from props

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import FloatingLabelInput, { setGlobalStyles, CustomLabelProps, Props as FloatingLabelProps, SetGlobalStyles as SetGlobalStylesProps } from './src';
+import FloatingLabelInput, { setGlobalStyles, CustomLabelProps, Props as FloatingLabelProps, InputRef as FloatingInputRef, SetGlobalStyles as SetGlobalStylesProps } from './src';
 
 export { FloatingLabelInput, setGlobalStyles };
-export type {CustomLabelProps, FloatingLabelProps, SetGlobalStylesProps };
+export type {CustomLabelProps, FloatingLabelProps, FloatingInputRef, SetGlobalStylesProps };
--- src/index.tsx
+++ src/index.tsx
@@ -152,9 +152,10 @@ const setGlobalStyles: SetGlobalStyles = {
   showCountdownStyles: undefined as TextStyle | undefined,
 };
 
-interface InputRef {
+export interface InputRef {
   focus(): void;
   blur(): void;
+  setNativeProps: (props: object) => void;
 }
 
 const AnimatedText = Animated.createAnimatedComponent(Text);
@@ -198,7 +199,6 @@ const FloatingLabelInput: React.ForwardRefRenderFunction<InputRef, Props> = (
     multiline,
     showPasswordImageStyles,
     value = '',
-    onSelectionChange,
     animationDuration,
     ...rest
   },
@@ -299,6 +299,9 @@ const FloatingLabelInput: React.ForwardRefRenderFunction<InputRef, Props> = (
     },
     blur() {
       inputRef.current.blur();
+    },
+    setNativeProps: (props: object) => {
+      inputRef.current.setNativeProps(props);
     },
   }));
 
